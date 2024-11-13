4663b9296503c8785cbf33c0bd51ac2ad16ffc83
aBeautifulBicycle <abeautifulbike@gmail.com>
2024-11-13 13:06:45 -0500

    slight css and js functionality increase

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
+let timer;
+
 function alertButton() {
     alert("Hello world!");
 }
 
 function buttonTest() {
-    document.getElementById("testButton").innerHTML = "Clicked!";
-    setTimeout(() => {
+    var button = document.getElementById("testButton");
+    if (button.innerHTML === "Not clicked!") {
+        button.innerHTML = "Clicked!";
+    } else if (button.innerHTML.substring(0,8) === "Clicked!") {
+        button.innerHTML = "Clicked x2!";
+    } else {
+        var numClicks = parseInt(button.innerHTML.substring(9, button.innerHTML.length - 1));
+        button.innerHTML = "Clicked x" + (numClicks + 1) + "!";
+    }
+    clearTimeout(timer);
+    timer = setTimeout(() => {
         document.getElementById("testButton").innerHTML = "Not clicked!";
     }, 10000);
 }
+
 
