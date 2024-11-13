d0a1a8a3683f19db676be2667e447de2c251997a
aBeautifulBicycle <abeautifulbike@gmail.com>
2024-11-13 11:56:55 -0500

    further testings

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 function alertButton() {
     alert("Hello world!");
 }
+
+function buttonTest() {
+    document.getElementById("testButton").innerHTML = "Clicked!";
+    setTimeout(() => {
+        document.getElementById("testButton").innerHTML = "Not clicked!";
+    }, 10000);
+}
+
